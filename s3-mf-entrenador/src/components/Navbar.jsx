e561a28b3205f7af84788c147dd2c289791e038e
harolte <harolte@hotmail.com>
2024-09-09 12:23:16 -0500

    s3-mf-entrenador/S2-HU-TP-28-HAROLT-v1.0

--- s3-mf-entrenador/src/components/Navbar.jsx
+++ s3-mf-entrenador/src/components/Navbar.jsx
@@ -1,11 +1,32 @@
-export default function Navbar(){
-    return(
-        <nav className="min-h-[10vh] flex justify-between p-2" style={{backgroundColor:"#FFFFFF"}}>
+import { useNavigate } from 'react-router-dom';
+
+export default function Navbar() {
+    const navigate = useNavigate();
+
+    return (
+        <nav className="min-h-[10vh] flex justify-between p-2" style={{ backgroundColor: "#FFFFFF" }}>
             <img src={"/logo-3.png"} alt="logo fia fit" className="w-[30vh] h-[10vh]" />
             <div className="flex flex-row items-center mr-4 gap-20">
-                <h1 className="text-[20px] font-bold" style={{color:"#4B4F57"}}>Inicio</h1>
-                <img src={"/icono-user.png"} alt="icono usuario" className="border border-white rounded-full w-[8vh] h-[6vh]"/>
+                {/* Alumnos Check-in */}
+                <div 
+                    className="flex flex-col text-center cursor-pointer"
+                    onClick={() => navigate('/check-in')}
+                >
+                    <h1 className="text-[20px] font-bold" style={{ color: "#4B4F57" }}>Alumnos</h1>
+                    <h1 className="text-[20px] font-bold" style={{ color: "#4B4F57" }}>Check - in</h1>
+                </div>
+
+                {/* Lista de Alumnos */}
+                <div 
+                    className="flex flex-col text-center cursor-pointer"
+                    onClick={() => navigate('/lista-alumnos')}
+                >
+                    <h1 className="text-[20px] font-bold" style={{ color: "#4B4F57" }}>Lista de</h1>
+                    <h1 className="text-[20px] font-bold" style={{ color: "#4B4F57" }}>Alumnos</h1>
+                </div>
+
+                <img src={"/icono-user.png"} alt="icono usuario" className="border border-white rounded-full w-[8vh] h-[6vh]" />
             </div>
         </nav>
-    )
+    );
 }
