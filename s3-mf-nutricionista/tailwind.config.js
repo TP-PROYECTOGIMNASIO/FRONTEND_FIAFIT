f75737186b024301e9391c4b7290ead2c595d0e5
harolte <harolte@hotmail.com>
2024-10-10 11:42:02 -0500

    HU-TP-S4-HAROLT1-v2.0 AB#600

---
+++ s3-mf-nutricionista/tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Asegúrate de que la ruta sea correcta y cubra tus archivos de componentes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
