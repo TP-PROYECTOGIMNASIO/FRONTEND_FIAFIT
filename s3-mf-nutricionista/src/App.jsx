254489467907b4984ff25ab4a62a9ce770075d9d
randyS30 <randy_saravia@usmp.pe>
2024-10-11 02:15:02 -0500

    HU-TP-S4-RANDY1

--- s3-mf-nutricionista/src/App.jsx
+++ s3-mf-nutricionista/src/App.jsx
@@ -3,10 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
 import './index.css'
-
+import DietPlanDetail from './pages/DietPlanDetail.jsx';
+import DietPlanList from './pages/DietPlanList.jsx';
 import HUVISUALLIZARINICIOSEGN from './pages/HUVISUALLIZARINICIOSEGN';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import PlanNutricion from './pages/PlanNutricion/PlanNutricion';
+import PlanForm from './pages/PlanForm/PlanForm.jsx';
 
 function App() {
 
@@ -17,7 +20,10 @@ function App() {
         <Navbar/>
         <Routes>
           <Route  path="/" element={<HUVISUALLIZARINICIOSEGN />} />
-
+          <Route  path="/Plan-Nutricion" element={<PlanNutricion />} />
+          <Route  path="/Plan-Form" element={<PlanForm />} />
+          <Route path="/plan" element={<DietPlanList />} />
+        <Route path="/plan/:dietPlanId" element={<DietPlanDetail />} />  
         </Routes>
         <Footer/>
         </>
