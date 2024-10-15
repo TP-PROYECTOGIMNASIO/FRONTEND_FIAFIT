c09383f5234e40c240c479b75a529b0c5e12e65f
moises1310M <moises_granados@usmp.pe>
2024-10-15 01:08:58 -0500

    HU-TP-S4-MOISES-v1.0 AB#642

--- s3-mf-fisioterapeuta/src/pages/HUVISUALLIZARINICIOSEGN.jsx
+++ s3-mf-fisioterapeuta/src/pages/HUVISUALLIZARINICIOSEGN.jsx
@@ -10,30 +10,33 @@ const HUVISUALLIZARINICIOSEGN = () => {
       />
 
       <div className="w-full max-w-4xl relative z-10"> {/* Añado relative y z-10 */}
-        <h1 className="text-4xl font-bold mb-12 text-red-600 text-center">
-          ¡Bienvenido Fisioterapeuta!
-        </h1>
 
         <div className="grid grid-cols-2 gap-12">
           <div className="text-left">
           </div>
 
-          <div className="text-left">
-            <div className="flex flex-col gap-4 bg-white p-4 rounded-b-lg shadow-lg">
-              <Link to={"/"} className="bg-white text-gray-600 border border-red-600 font-semibold py-2 px-4 rounded-lg">
-                <h3 className="text-lg text-center">EMPEZAR PLAN</h3>
-                <h1 className="text-3xl text-center">TRATAMIENTO →</h1>
-              </Link>
-              <Link to={"/"}className="bg-white text-gray-600 border border-red-600 font-semibold py-2 px-4 rounded-lg">
-                <h3 className="text-lg text-center">REGISTRAR</h3>
-                <h1 className="text-3xl text-center">EJERCICIOS →</h1>
-              </Link>
-            </div>
-          </div>
+          <div className="flex flex-col bg-white p-4 rounded-b-lg shadow-lg">
+  <div className="text-left mb-6"> {/* Aumentar el mb para más espacio */}
+    <Link to={"/"} className="bg-white text-red-600 font-semibold py-2 px-4 rounded-lg">
+      <h3 className="text-3xl text-center">EMPEZAR PLAN</h3>
+      <h1 className="text-3xl text-center">ENTRENAMIENTO</h1>
+    </Link>
+  </div>
+
+  <div className="text-left">
+    <Link to={"/"} className="bg-white text-red-600 font-semibold py-2 px-4 rounded-lg">
+      <h3 className="text-3xl text-center">REGISTRAR</h3>
+      <h1 className="text-3xl text-center">EJERCICIOS</h1>
+    </Link>
+  </div>
+</div>
+
+
         </div>
       </div>
     </div>
   );
 };
+
 
 export default HUVISUALLIZARINICIOSEGN;
