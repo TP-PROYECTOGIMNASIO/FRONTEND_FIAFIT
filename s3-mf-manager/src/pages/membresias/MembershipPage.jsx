d9581904a7a295bbe642c0715b1ecf441913b6a3
moises1310M <moises_granados@usmp.pe>
2024-11-11 17:15:18 -0500

    HU-TP-S6-MOISES-v3.0 HU78y79

--- s3-mf-manager/src/pages/membresias/MembershipPage.jsx
+++ s3-mf-manager/src/pages/membresias/MembershipPage.jsx
@@ -91,7 +91,7 @@ const MembershipPage = () => {
       <div className="flex justify-between items-center mb-8">
         <button
           onClick={() => window.history.back()}
-          className="text-gray-500 hover:text-gray-700 flex items-center">
+          className="text-black-500 hover:text-gray-700 flex items-center">
           <i className="fas fa-arrow-left mr-2"></i> Regresar
         </button>
 
